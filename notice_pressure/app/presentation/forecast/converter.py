a19b031749a95be6fa4d786bea789e3c6e0731ae
j-azuma <azumanotdetail@gmail.com>
2022-07-31 10:02:16 +0900

    docstringを追加する

--- notice_pressure/app/presentation/forecast/converter.py
+++ notice_pressure/app/presentation/forecast/converter.py
@@ -6,6 +6,14 @@ from app.forecast import ForecastDto
 class Converter:
     @staticmethod
     def to_dict(daily_forecast: List[ForecastDto]) -> dict:
+        """ユースケース層から渡されたDTOのリストをdictに変換する
+
+        Args:
+            daily_forecast (List[ForecastDto]): DTO
+
+        Returns:
+            dict: 辞書形式のデータ
+        """
         daily_forecast_value: List[dict] = [
             element.__dict__ for element in daily_forecast
         ]
