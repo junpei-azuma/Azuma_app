68534868bf78fdcc8fce74a8df9cf0c36a6f86c3
j-azuma <azumanotdetail@gmail.com>
2022-08-14 16:57:11 +0900

    予報メール本文クラス

--- notice_pressure/app/mail/forecastmailbody.py
+++ notice_pressure/app/mail/forecastmailbody.py
@@ -4,9 +4,46 @@ from typing import List
 
 class ForecastMailBody:
     def __init__(self, daily_forecast: List[dict]) -> None:
+        """インスタンスを生成する。
+           本文全体の生成はここでは行わない
+
+        Args:
+            daily_forecast (List[dict]): _description_
+        """
         self.__headline: str = self._set_headline()
-        self.__forecast_content: str
-        self.__body: str
+        self.__forecast_content: str = self._set_main(daily_forecast)
+
+    @property
+    def headline(self) -> str:
+        """インスタンス変数を隠蔽する。
+
+        Returns:
+            str: _description_
+        """
+        return self.__headline
+
+    @property
+    def forecast_content(self) -> str:
+        """インスタンス変数を隠蔽する。
+
+        Returns:
+            str: _description_
+        """
+        return self.__forecast_content
+
+    def compose(self) -> str:
+        """見出しとメインコンテンツからメール本文を生成する。
+           メール本文の例:
+
+           08/15 (Mon) の気圧予想です。
+
+           06:00: 1000hPa(2)
+           09:00: 999hPa(-1)
+           ⋮
+        Returns:
+            str: _description_
+        """
+        return self.headline + "\n\n" + self.forecast_content
 
     @staticmethod
     def _set_headline() -> str:
--- notice_pressure/tests/unit/mail/test_forecastmailbody.py
+++ notice_pressure/tests/unit/mail/test_forecastmailbody.py
@@ -2,6 +2,8 @@ from typing import List
 from app.mail import ForecastMailBody
 import pytest
 
+from app.mail import ForecastMailBody
+
 
 @pytest.mark.freeze_time("2022-08-14")
 def test_本文見出しを生成():
@@ -24,3 +26,24 @@ def test_本文のメインコンテンツを生成():
     main_content: str = ForecastMailBody._set_main(daily_forecast)
 
     assert main_content == "06:00: 1000hPa(1)\n09:00: 1003hPa(3)\n12:00: 1002hPa(-1)"
+
+
+@pytest.mark.freeze_time("2022-08-14")
+def test_本文を生成():
+    # 事前準備： ForecastMailBodyインスタンスを生成する。
+    daily_forecast: List[dict] = [
+        {"date": "06:00", "pressure": "1000", "difference": "1"},
+        {"date": "09:00", "pressure": "1003", "difference": "3"},
+        {"date": "12:00", "pressure": "1002", "difference": "-1"},
+    ]
+
+    forecastmailbody: ForecastMailBody = ForecastMailBody(daily_forecast)
+
+    # 操作： メール本文を生成
+    body: str = forecastmailbody.compose()
+
+    # 想定結果： メール本文が正しく作成されている。
+    assert (
+        body
+        == "08/15 (Mon) の気圧予想です。\n\n06:00: 1000hPa(1)\n09:00: 1003hPa(3)\n12:00: 1002hPa(-1)"
+    )
